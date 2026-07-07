e28c543acd79f97ff97cc1d4fd8231a2018af444
Akhilesh Chaudhary <akhileshchy9910@gmail.com>
2026-07-07 23:48:06 +0530

    Disable flutter analytics to prevent Vercel hang

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ echo "Downloading Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
+# Disable analytics to prevent interactive prompts that hang the build
+flutter config --no-analytics
+
 echo "Running flutter pub get..."
 flutter pub get
 
